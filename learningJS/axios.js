b095898df3a9f72a1ca4c043d15aefc4eeaa76c8
Quang Ngoc <ngocquang2509@gmail.com>
2022-05-05 17:34:00 +0700

    practice axios

---
+++ learningJS/axios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+/* GET request */
+axios
+  .get("/user?ID=12345")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+/* POST request */
+axios
+  .post("/users", {
+    firstName: "First name",
+    lastName: "Last name",
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
