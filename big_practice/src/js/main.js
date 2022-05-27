e2abc1606added0f3207877c6a97e80b9e6a3962
Quang Ngoc <ngocquang2509@gmail.com>
2022-05-27 15:27:49 +0700

    remove unnecessary code

--- big_practice/src/js/main.js
+++ big_practice/src/js/main.js
@@ -6,21 +6,3 @@ const model = new TaskModel(),
   view = new TaskView();
 
 const app = new TaskController(model, view);
-
-var modal = document.getElementsByClassName("popup__content");
-var btn = document.getElementsByClassName("btn add");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
