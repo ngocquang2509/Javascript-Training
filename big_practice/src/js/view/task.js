152130e6ffe16b7258ee05324f51efac00e03d91
Quang Ngoc <ngocquang2509@gmail.com>
2022-05-27 17:22:12 +0700

    implement add modal (in-process)

--- big_practice/src/js/view/task.js
+++ big_practice/src/js/view/task.js
@@ -1,15 +1,19 @@
 export default class TaskView {
   constructor() {
-    this.app = this.getElement(".root");
-    this.form = this.createElement("form");
-    this.input = this.createElement("input");
-    this.input.type = "text";
-    this.input.placeholder = "Task Name";
-    this.input.name = "task";
-    this.createButton = this.createElement("button");
-    this.createButton.textContent = "Create";
-    this.list = this.createElement("ul", "list");
-    this.app.append(this.form);
+    // this.app = this.getElement(".root");
+    // this.form = this.createElement("form");
+    // this.input = this.createElement("input");
+    // this.input.type = "text";
+    // this.input.placeholder = "Task Name";
+    // this.input.name = "task";
+    // this.createButton = this.createElement("button");
+    // this.createButton.textContent = "Create";
+    // this.list = this.createElement("ul", "list");
+    // this.app.append(this.form);
+
+    this.inputName = document.getElementById("add-name");
+    this.inputDescripion = document.getElementById("add-description");
+    this.addBtn = document.getElementById("submit");
   }
 
   createElement(tag, className) {
@@ -25,4 +29,22 @@ export default class TaskView {
 
     return element;
   }
+
+  // var modal = document.getElementsByClassName("popup__content");
+  // var btn = document.getElementsByClassName("btn add");
+  // var span = document.getElementsByClassName("close")[0];
+
+  // btn.onclick = function () {
+  //   modal.style.display = "block";
+  // };
+
+  // span.onclick = function () {
+  //   modal.style.display = "none";
+  // };
+
+  // window.onclick = function (event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // };
 }
