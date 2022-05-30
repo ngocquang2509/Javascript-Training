e0ce537b7985fdfdbe36ca594a7817f2bd93b8dc
Quang Ngoc <ngocquang2509@gmail.com>
2022-05-30 17:09:47 +0700

    update view

--- big_practice/src/js/view/task.js
+++ big_practice/src/js/view/task.js
@@ -14,6 +14,8 @@ export default class TaskView {
     this.inputName = document.getElementById("add-name");
     this.inputDescripion = document.getElementById("add-description");
     this.addBtn = document.getElementById("submit");
+
+    this._temporaryTaskText = "";
   }
 
   createElement(tag, className) {
@@ -28,6 +30,57 @@ export default class TaskView {
     const element = document.querySelector(selector);
 
     return element;
+  }
+
+  display(tasks) {
+    if (tasks.length !== 0) {
+      const wrapper = document.getElementsByClassName("task-list");
+      wrapper[0].innerHTML = "";
+      console.log("wrapper", wrapper);
+
+      tasks.forEach((task) => {
+        const li = this.createElement("li");
+        li.id = task.id;
+
+        const checkbox = this.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = task.complete;
+        checkbox.id = li.id;
+
+        if (task.complete) {
+          const strike = this.createElement("s");
+          strike.textContent = task.text;
+          span.append(strike);
+        } else {
+          span.textContent = task.text;
+        }
+
+        // Append nodes
+        this.todoList.append(li);
+      });
+    }
+  }
+
+  bindAddTask(handleAddTask) {
+    this.input.addEventListener("keyup", (e) => {
+      if (e.which === this.ENTER_KEY) {
+        if (this._taskText) {
+          handleAddTask(this._taskText);
+          this._resetInput();
+        }
+      }
+    });
+  }
+
+  bindUpdateTask(handler) {
+    this.taskList.addEventListener("focusout", (e) => {
+      if (this._temporaryTaskText) {
+        const id = e.target.parentElement.id;
+
+        handler(id, this._temporaryTaskText);
+        this._temporaryTaskText = "";
+      }
+    });
   }
 
   // var modal = document.getElementsByClassName("popup__content");
