5ade96e993e85d156b955e4b386782e074b5528a
Quang Ngoc <ngocquang2509@gmail.com>
2022-05-30 17:09:55 +0700

    update controller

--- big_practice/src/js/controller/task.js
+++ big_practice/src/js/controller/task.js
@@ -2,5 +2,24 @@ export default class TaskController {
   constructor(taskModel, taskView) {
     this.taskModel = taskModel;
     this.taskView = taskView;
+
+    this.taskView.bindAddTask(this.handleAddTask);
+    this.taskView.bindUpdateTodo(this.handleUpdateTask);
   }
+
+  init = async () => {
+    const tasks = await this.model.getTask();
+    console.log("tasks", tasks);
+    this.view.displayTodos(tasks);
+  };
+
+  handleAddTask = async (taskText) => {
+    const tasks = await this.model.addTask(taskText);
+    this.view.displayTasks(tasks);
+  };
+
+  handleUpdateTask = async (id, taskText) => {
+    const tasks = await this.model.updateTodo(id, taskText);
+    this.view.displayTasks(tasks);
+  };
 }
