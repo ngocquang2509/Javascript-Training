adde8993a324a05b00f14cac661961f2eee1df0c
Quang Ngoc <ngocquang2509@gmail.com>
2022-06-07 16:43:54 +0700

    remove comment, add init()

--- big_practice/src/js/controllers/task.js
+++ big_practice/src/js/controllers/task.js
@@ -1,22 +1,34 @@
-export default class TaskController {
+export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
 
+    this.model.bindTaskListChanged(this.onTaskListChanged);
     this.view.bindAddTask(this.handleAddTask);
+    // this.view.bindUpdateTask(this.handleUpdateTask);
+    this.view.bindDeleteTask(this.handleDeleteTask);
     this.onTaskListChanged(this.model.showTask);
   }
 
-  onTaskListChanged = (tasks) => {
-    this.view.display(tasks);
-  };
-
-  handleAddTask = (name, description, date) => {
-    this.model.addTask(name, description, date);
-  };
-
-  handleUpdateTask = async (id, taskText) => {
-    const tasks = await this.model.updateTodo(id, taskText);
+  init = async () => {
+    const tasks = await this.model.showTask();
     this.view.displayTasks(tasks);
+  };
+
+  onTaskListChanged = (tasks) => {
+    this.view.displayTasks(tasks);
+  };
+
+  handleAddTask = (name, description) => {
+    this.model.addTask(name, description);
+  };
+
+  // handleUpdateTask = async (id, taskText) => {
+  //   const tasks = await this.model.updateTodo(id, taskText);
+  //   this.view.displayTasks(tasks);
+  // };
+
+  handleDeleteTask = (id) => {
+    this.model.deleteTask(id);
   };
 }
