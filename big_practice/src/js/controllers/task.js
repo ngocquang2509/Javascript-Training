cf107efbb5117a21d5ff12ac4f3debb85aa7974a
Quang Ngoc <ngocquang2509@gmail.com>
2022-06-27 17:25:17 +0700

    register edit task function

--- big_practice/src/js/controllers/task.js
+++ big_practice/src/js/controllers/task.js
@@ -5,15 +5,14 @@ export default class Controller {
 
     this.model.bindTaskListChanged(this.onTaskListChanged);
     this.view.bindAddTask(this.handleAddTask);
-    this.view.editTaskModal(this.getTaskById);
-    //this.view.bindUpdateTask(this.handleUpdateTask);
+    this.view.getTaskById(this.getTaskById);
+    this.view.bindEditTask(this.handleEditTask);
     this.view.bindDeleteTask(this.handleDeleteTask);
   }
 
   init = async () => {
     const tasks = await this.model.getTask();
     this.onTaskListChanged(this.model.tasks);
-    return tasks;
   };
 
   onTaskListChanged = (tasks) => {
@@ -27,18 +26,18 @@ export default class Controller {
 
   getTaskById = (id) => {
     const task = this.model.getTaskById(id);
-    console.log("taskCtrl", task);
+    //console.log("taskCtrl", task);
     return task;
-    //return this.model.getTaskById(id);
   };
 
-  // handleUpdateTask = async (id, name, description) => {
-  //   const tasks = await this.model.updateTask(id, name, description);
-  //   this.view.displayTasks(tasks);
-  // };
+  handleEditTask = async (id, name, description) => {
+    const tasks = await this.model.updateTask(id, name, description);
+    this.onTaskListChanged(this.model.tasks);
+  };
 
   handleDeleteTask = async (id) => {
-    const tasks = await this.model.deleteTask(id);
-    this.view.displayTasks(tasks);
+    const task = await this.model.deleteTask(id);
+    // this.view.displayTasks(tasks);
+    this.onTaskListChanged(this.model.tasks);
   };
 }
